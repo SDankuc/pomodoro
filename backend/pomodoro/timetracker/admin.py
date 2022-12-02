16f88fb3f88b960be04d5394aff264e3a138695c
Stefan <stefan.dankuc.dev@gmail.com>
2022-12-02 09:20:39 +0100

    2 - As Pomodoro app I want Models for Schema/Pomodoro/Category objects

---
+++ backend/pomodoro/timetracker/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Schema,Pomodoro,Category
+
+class SchemaAdmin(admin.ModelAdmin):
+    list_filter = ("id","duration")
+    list_display = ("active")
+
+class PomodoroAdmin(admin.ModelAdmin):
+    list_filter = ("title")
+    list_display = ("title", "amount")
+
+class CategorykAdmin(admin.ModelAdmin):
+    list_filter = ("title")
+    list_display = ("title")
+
+admin.site.register(Schema,SchemaAdmin)
+admin.site.register(Pomodoro,PomodoroAdmin)
+admin.site.register(Category,CategorykAdmin)

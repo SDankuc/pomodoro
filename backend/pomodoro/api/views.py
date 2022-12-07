2d9c0f22bf8387300f83dbd4ec1008a211a87351
Stefan <stefan.dankuc.dev@gmail.com>
2022-12-07 13:05:45 +0100

    35-As User I want to use POST method for changing the amount on Pomodoro object

--- backend/pomodoro/api/views.py
+++ backend/pomodoro/api/views.py
@@ -3,6 +3,7 @@ from .serializers import SchemaSerializer, CategorySerializer, PomodoroSerialize
 from timetracker.models import Schema, Category, Pomodoro
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
@@ -76,20 +77,22 @@ class PomodoroRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     def perform_update(self, serializer):
         isinstance = serializer.save()
 
-class PomodoroUpdateAmount(generics.RetrieveUpdateAPIView):
-    queryset = Pomodoro.objects.all()
-    serializer_class = PomodoroAmountSerializer
-    permission_classes = (IsAuthenticated,)
-    lookup_field = "pk"
-
-    def perform_update(self, serializer):
-        done = serializer.validated_data.get("done")
-        amount = serializer.validated_data.get("amount")
+@api_view(['POST'])
+def pomodoro_amount(request,id):
+    pomodoro = Pomodoro.objects.filter(id = id).first()
+    amount = pomodoro.amount
+    if amount > 0:
+        amount -= 1
         if amount == 0:
             done = True
-            serializer.save(done=done, amount=amount)
-        elif amount < 0:
-            return Response({"Invalid": "amount can not be less than 0"}, status=400)
+            pomodoro.amount = amount
+            pomodoro.done = True
+            pomodoro.save()
         else:
-            done = False
-            serializer.save(done=done, amount=amount)
+            pomodoro.amount = amount
+            pomodoro.save()
+        context = {"id":pomodoro.id,"category_id": pomodoro.category_id.id,"title":pomodoro.title,"amount":pomodoro.amount,"done":pomodoro.done,}
+        return Response(context)
+    else:
+        context = {"id":pomodoro.id,"category_id": pomodoro.category_id.id,"title":pomodoro.title,"amount":pomodoro.amount,"done":pomodoro.done,}
+        return Response(context)
